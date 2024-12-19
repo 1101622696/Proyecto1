e70b49c67b684f112b90bd8b48e05bd034c8bc0b
Dcardenas <126295398+1101622696@users.noreply.github.com>
2024-12-19 14:40:58 -0500

    Update formulario.js

--- formulario/formulario.js
+++ formulario/formulario.js
@@ -1,184 +1,109 @@
-const persona = [];
 
-let op = null
-let indice=null
+  var firmaBase64 = '';
 
+  window.onload = function () {
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var isDrawing = false;
+    var lastX = 0;
+    var lastY = 0;
 
+    // Set initial canvas background
+    context.fillStyle = "#FFFFFF";
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('touchstart', startDrawingTouch);
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('touchmove', drawTouch);
+    canvas.addEventListener('mouseup', stopDrawing);
+    canvas.addEventListener('touchend', stopDrawing);
+    canvas.addEventListener('mouseout', stopDrawing);
 
-function formulario() {
-
-    let nombre=document.getElementById("nombre").value;
-    let apellido=document.getElementById("apellido").value;
-    let genero=document.querySelector('input[name="genero"]:checked').value;
-    let correo=document.getElementById("correo").value;
-    let fecha=document.getElementById("fecha").value;
-    let opciones=document.getElementById("opciones").value;
-    let ndoc=document.getElementById("ndoc").value;
-    let numtel=document.getElementById("numtel").value;
-
-    if (op===true){
-      persona[indice].nombre=document.getElementById("nombre").value
-      persona[indice].apellido=document.getElementById("apellido").value
-      persona[indice].genero=document.querySelector('input[name="genero"]:checked').value
-      persona[indice].correo=document.getElementById("correo").value
-      persona[indice].fecha=document.getElementById("fecha").value
-      persona[indice].opciones=document.getElementById("opciones").value
-      persona[indice].ndoc=document.getElementById("ndoc").value
-      persona[indice].numtel=document.getElementById("numtel").value
-    }else{
-let usuario = {
-  nombre: nombre,
-  apellido: apellido,
-  genero: genero,
-  correo: correo,
-  fecha: fecha,
-  opciones: opciones,
-  ndoc: ndoc,
-  numtel: numtel
-}
-    
-persona.push(usuario)
+    function startDrawing(e) {
+      isDrawing = true;
+      [lastX, lastY] = [e.offsetX, e.offsetY];
     }
-document.getElementById("nombre").value = " ";
-document.getElementById("apellido").value = " ";
-document.getElementById("hombre").checked = false ;
-document.getElementById("mujer").checked = false;
-document.getElementById("correo").value = " ";
-document.getElementById("fecha").value = " ";
-document.getElementById("opciones").value = " ";
-document.getElementById("ndoc").value = " ";
-document.getElementById("numtel").value = " ";
 
-console.log(persona);
+    function startDrawingTouch(e) {
+      isDrawing = true;
+      var rect = canvas.getBoundingClientRect();
+      [lastX, lastY] = [e.touches[0].clientX - rect.left, e.touches[0].clientY - rect.top];
+      e.preventDefault();
+    }
 
-}
+    function draw(e) {
+      if (!isDrawing) return;
+      drawLine(lastX, lastY, e.offsetX, e.offsetY);
+      [lastX, lastY] = [e.offsetX, e.offsetY];
+    }
 
-function validar() {
-      let vericorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    function drawTouch(e) {
+      if (!isDrawing) return;
+      var rect = canvas.getBoundingClientRect();
+      drawLine(lastX, lastY, e.touches[0].clientX - rect.left, e.touches[0].clientY - rect.top);
+      [lastX, lastY] = [e.touches[0].clientX - rect.left, e.touches[0].clientY - rect.top];
+      e.preventDefault();
+    }
 
-  if (document.getElementById("nombre").value == "") {
-      document.getElementById("alert").textContent = "Por favor el nombre esta vacio"
-      setTimeout(() => {
-          document.getElementById("alert").textContent = ""
-      }, 3000);
-  } else if (document.getElementById("apellido").value == "") {
-      document.getElementById("alert").textContent = "Por favor el apellido esta vacio"
-      setTimeout(() => {
-          document.getElementById("alert").textContent = ""
-      }, 3000);
-    }else if (!document.querySelector('input[name="genero"]:checked')) {
-        document.getElementById("alert").textContent = "Por favor seleccione un genero"
-        setTimeout(() => {
-            document.getElementById("alert").textContent = ""
-        }, 3000);
-    }else if (!vericorreo.test(document.getElementById("correo").value)) {
-      document.getElementById("alert").textContent = "Por favor el correo esta vacio"
-      setTimeout(() => {
-          document.getElementById("alert").textContent = ""
-      }, 3000);
-    }else if ((new Date().getFullYear() - new Date(document.getElementById("fecha").value).getFullYear()) < 18)  {
-      document.getElementById("alert").textContent ="debe ser mayor para poder registrarse" 
-      setTimeout(() => {
-          document.getElementById("alert").textContent = ""
-      }, 3000);
-    } else if (document.getElementById("opciones").value == "selecciona un tipo de documento") {
-      document.getElementById("alert").textContent = "Por favor seleccione una opcion"
-      setTimeout(() => {
-          document.getElementById("alert").textContent = ""
-      }, 3000);
-    } else if (document.getElementById("ndoc").value.length<7) {
-        document.getElementById("alert").textContent = "Por favor escriba su número de documento"
-        setTimeout(() => {
-            document.getElementById("alert").textContent = ""
-        }, 3000);
-      } else if (document.getElementById("numtel").value.length<10) {
-        document.getElementById("alert").textContent = "Por favor escriba su número de telefono"
-        setTimeout(() => {
-            document.getElementById("numtel").textContent = ""
-        }, 3000)
-  } else {
-      formulario();
+    function stopDrawing() {
+      isDrawing = false;
+    }
 
-     document.getElementById("tabla").innerHTML=""
-pintar()
-op=false
+    function drawLine(x1, y1, x2, y2) {
+      context.beginPath();
+      context.moveTo(x1, y1);
+      context.lineTo(x2, y2);
+      context.strokeStyle = '#000';
+      context.lineWidth = 2;
+      context.stroke();
+      context.closePath();
+    }
+  };
 
+  function guardarDatos() {
+    var nombre = document.getElementById('nombre').value.trim();
+    var cargo = document.getElementById('cargo').value.trim();
+    var cedula = document.getElementById('cedula').value.trim();
+    var pregunta1 = document.getElementById('pregunta1').value.trim();
+    var antiguedad = document.getElementById('antiguedad').value.trim();
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
 
+    firmaBase64 = canvas.toDataURL();
+
+    if (!nombre || !cargo || !cedula || !pregunta1 || !antiguedad) {
+      alert('Por favor, complete todos los campos del formulario.');
+      return false;
+    }
+
+    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    var pixels = imageData.data;
+    var tieneFirma = Array.from(pixels).some((pixel, i) => i % 4 === 3 && pixel !== 0);
+
+    if (!tieneFirma) {
+      alert('Por favor, firme antes de enviar el formulario.');
+      return false;
+    }
+
+    google.script.run.withSuccessHandler(function() {
+      document.getElementById('nombre').value = '';
+      document.getElementById('cargo').value = '';
+      document.getElementById('cedula').value = '';
+      document.getElementById('pregunta1').value = '';
+      document.getElementById('antiguedad').value = '';
+      limpiarCanvas();
+      alert('Formulario Enviado');
+    }).guardarDatosYFirma(nombre, cargo, cedula, pregunta1, firmaBase64, antiguedad);
+
+    return false;
   }
-}
 
-function pintar(){
-  let frag= document.createDocumentFragment()
-
-
-  persona.forEach((item, index) => {
-    let tr = document.createElement("tr")
-    let td1 = document.createElement("td")
-    let td2 = document.createElement("td")
-    let td3 = document.createElement("td")
-    let td4 = document.createElement("td")
-    let td5 = document.createElement("td")
-    let td6 = document.createElement("td")
-    let td7 = document.createElement("td")
-    let td8 = document.createElement("td")
-    let td9 = document.createElement("td")
-    let editar = document.createElement("button")
-    let eliminar = document.createElement("button")
-    editar.textContent = "📝"
-    editar.addEventListener("click",()=>{
-      edita(item, index)
-    })
-    eliminar.textContent="❌"
-    eliminar.addEventListener("click",()=>{
-    borrar(index)
-  })
-    td1.textContent=item.nombre
-    td2.textContent=item.apellido
-    td4.textContent=item.genero
-    td3.textContent=item.correo
-    td5.textContent=item.opciones
-    td6.textContent=item.ndoc
-    td7.textContent=item.fecha
-    td8.textContent=item.numtel
-    td9.appendChild(editar)
-    td9.appendChild(eliminar)
-    tr.appendChild(td1)
-    tr.appendChild(td2)
-    tr.appendChild(td3)
-    tr.appendChild(td4)
-    tr.appendChild(td5)
-    tr.appendChild(td6)
-    tr.appendChild(td7)
-    tr.appendChild(td8)
-    tr.appendChild(td9)
-    frag.appendChild(tr)
-    document.getElementById("tabla").appendChild(frag)
-  })
-}
-
-function edita(r, i){
-  op =  true
-  indice=i
-  console.log(r);
-  document.getElementById("nombre").value=r.nombre
-     document.getElementById("apellido").value=r.apellido
-    document.getElementById("correo").value=r.correo
-    if(r.genero==="hombre"){
-      document.getElementById("hombre").checked=true
-    }else{
-      document.getElementById("mujer").checked=true
-    }
-
-    document.getElementById("opciones").value=r.opciones
-    document.getElementById("ndoc").value=r.ndoc
-    document.getElementById("fecha").value=r.fecha
-    document.getElementById("numtel").value=r.numtel
-
-}
-function borrar(i){
-  index=i
-  persona.splice(index, 1);
-  document.getElementById("tabla").innerHTML=""
-pintar()
-}
+  function limpiarCanvas() {
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "#FFFFFF";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    firmaBase64 = '';
+  }
